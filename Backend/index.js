35d523629e2bffcb11d2f957c6da364543798b5e
Kanika-2004 <155616397+Kanika-2004@users.noreply.github.com>
2025-02-13 21:28:55 +0530

    site changed

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 const uri = "mongodb+srv://kanikadebnath8759:uM6vVxAven34GOU6@cluster0.wopok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const corsOptions = {
-  origin: 'https://hackitup-frontend.vercel.app',
+  origin: 'https://hackitup-frontend-br48qwp17-kanika-debnaths-projects.vercel.app/',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
